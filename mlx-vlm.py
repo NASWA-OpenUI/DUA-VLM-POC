b708110d9b36447c04e691dae3ad0b25df810292
Dan Hon <dan@danhon.com>
2025-07-29 12:23:53 -0700

    Initial commit

---
+++ mlx-vlm.py
@@ -0,0 +1,80 @@
+import mlx.core as mx
+from mlx_vlm import load, generate
+from mlx_vlm.prompt_utils import apply_chat_template
+from mlx_vlm.utils import load_config
+from PIL import Image
+from pathlib import Path
+from datetime import datetime
+
+
+import csv
+
+MODELS_FILE = "models.txt"
+OUTPUT_FILE = "results.csv"
+TEST_PROMPT = "Provide the form number, year,  proprietor name,  principal crop or activity, and net profit or loss as json"
+TEST_IMAGE = "images/test-002.jpg"
+IMG_PATH = Path(TEST_IMAGE)
+
+def load_models_list(models_file):
+    """Load the models from the models file"""
+    with open(models_file, "r") as f:
+        return [line.strip() for line in f if line.strip()]
+
+def run_prompt_on_model(model_path, prompt, image):
+    model, processor = load(model_path)
+    config = load_config(model_path)
+    formatted_prompt = apply_chat_template(processor, config, prompt, num_images=len(image))
+    response = generate(model, processor, formatted_prompt, image, verbose=False)
+    return response
+
+
+def main():
+    models = load_models_list(MODELS_FILE)
+    results = []
+    # image = ["TEST_IMAGE"]
+    image = [Image.open(IMG_PATH)]
+    prompt = TEST_PROMPT
+
+    for model_path in models:
+        print(f"Running prompt on {model_path}...")
+        timestamp = datetime.now().isoformat(timespec="seconds")
+        
+        try: 
+            output = run_prompt_on_model(model_path, prompt, image)
+            results.append({"timestamp": timestamp, "model": model_path, "prompt": prompt, "output": output.text})
+        except Exception as e:
+            results.append({"model": model_path, "output": f"Error {e}"})
+
+
+    # write to CSV
+    with open(OUTPUT_FILE, "a", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["timestamp", "model", "prompt", "output"])
+        # writer.writeheader()
+        writer.writerows(results)
+
+    print(f"Results saved to {OUTPUT_FILE}")
+
+# # Load the model
+# model_path = "mlx-community/Qwen2-VL-2B-Instruct-4bit"
+# model, processor = load(model_path)
+# config = load_config(model_path)
+
+# Prepare input
+# image = ["http://images.cocodataset.org/val2017/000000039769.jpg"]
+
+# image = ["images/test-002.jpg"]
+# image = [Image.open("...")] can also be used with PIL.Image.Image objects
+# prompt = "Provide the form number, year,  proprietor name,  principal crop or activity, and net profit or loss as json"
+
+# # Apply chat template
+# formatted_prompt = apply_chat_template(
+#     processor, config, prompt, num_images=len(image)
+# )
+
+# # Generate output
+# output = generate(model, processor, formatted_prompt, image, verbose=False)
+# print(output)
+
+
+if __name__ == "__main__":
+    main()
